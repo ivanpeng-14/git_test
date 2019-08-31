4f6229cadc4fa2ba5ea032857bc09ec3b53e2a19
Ivan Peng <ipeng@usc.edu>
2019-08-31 00:47:52 -0700

    removed test.cpp and edited HelloWorld.cpp

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,9 +1,21 @@
 # include <iostream>
 
 using namespace std;
+
+int max(int a, int b) {
+	int result;
+	if (a > b) 
+		result = a;
+	else 
+		result = b;
+	return result;
+}
+
 int main() 
 {
+	int x = 3, y = 6;
   for (int i = 0; i < 10; i++) 
     cout << "Fight On!" << " " << i << endl;
+	cout << max(x, y) << endl;
   return 0;
 }
